7622a2e562390dad1f1ede2b153a53f58e51da3f
tkm404 <tmcdowellddhs@yahoo.com>
2023-03-24 15:07:00 -0700

    WIP method 1 a bust, trying hex-to-string method 2.

--- js/scripts.js
+++ js/scripts.js
@@ -31,19 +31,7 @@ window.addEventListener("load", function() {
         color1.removeAttribute("class");
         document.querySelector("span#answer3").innerText = favoriteColor;
         // string-to-hex converter (wish me luck!)
-        let stringToColor = function(str) {
-          const hash = 0;
-          for (const i = 0; i < str.length; i++) {
-            hash = str.charCodeAt(i) + ((hash << 5) - hash);
-          }
-        let colour = '#';
-        for (const i = 0; i < 3; i++) {
-          const value = (hash >> (i * 8)) & 0xFF;
-          colour += ('00' + value.toString(16)).substr(-2)
-        }
-        return colour;
-        }
-        document.querySelector("span#hexReturn").innerText = stringToColor
+     
         //
         div4.removeAttribute("class");
         let form4 = document.querySelector("form#vacationChoice");
