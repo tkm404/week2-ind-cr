bbbc54e6334d0f5b667108982227b2a2e180ff2b
tkm404 <tmcdowellddhs@yahoo.com>
2023-03-24 10:52:42 -0700

    prepare js script for question 3.

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,34 @@ window.addEventListener("load", function() {
   let form1 = document.querySelector("form#dropSurvey");
   let animal1 = document.querySelector("h3#animal1");
   let div2 = document.querySelector("div#secondQuest");
+  //
   form1.addEventListener("submit", function(event) {
     event.preventDefault();
-    //code inside function
+    //
     const animalSelect = document.getElementById("Animals").value;
     animal1.removeAttribute("class");
     document.querySelector("span#answer1").innerText = animalSelect;
     div2.removeAttribute("class");
     let form2 = document.querySelector("form#superHero");
-    let hero1 = document.querySelector("h3#hero1")
+    let hero1 = document.querySelector("h3#hero1");
+    let div3 = document.querySelector("div#thirdQuest");
+    //
     form2.addEventListener("submit", function(event) {
       event.preventDefault();
+      //
       const heroSelect = document.getElementById("heroes").value;
       hero1.removeAttribute("class");
       document.querySelector("span#answer2").innerText = heroSelect;
+      div3.removeAttribute("class");
+      let form3 = document.querySelector("form#favColor");
+      let color1 = document.querySelector("h3#color1");
+      let div4 = document.querySelector("div#fourthQuest");
+      //
+      form3.addEventListener("submit", function(event) {
+        event.preventDefault();
+        //
+      })
+
     })
   })
 });
