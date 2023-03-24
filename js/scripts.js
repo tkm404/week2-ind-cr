fea70092136e72a121f0c98003a586900a080eb8
tkm404 <tmcdowellddhs@yahoo.com>
2023-03-24 14:58:51 -0700

    mount string-to-hex converter (fingers crossed)

--- js/scripts.js
+++ js/scripts.js
@@ -30,6 +30,21 @@ window.addEventListener("load", function() {
         const favoriteColor = document.getElementById("colorWord").value;
         color1.removeAttribute("class");
         document.querySelector("span#answer3").innerText = favoriteColor;
+        // string-to-hex converter (wish me luck!)
+        let stringToColor = function(str) {
+          const hash = 0;
+          for (const i = 0; i < str.length; i++) {
+            hash = str.charCodeAt(i) + ((hash << 5) - hash);
+          }
+        let colour = '#';
+        for (const i = 0; i < 3; i++) {
+          const value = (hash >> (i * 8)) & 0xFF;
+          colour += ('00' + value.toString(16)).substr(-2)
+        }
+        return colour;
+        }
+        document.querySelector("span#hexReturn").innerText = stringToColor
+        //
         div4.removeAttribute("class");
         let form4 = document.querySelector("form#vacationChoice");
         let vacation1 = document.querySelector("h3#vacation1");
