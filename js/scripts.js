d21dc7a30263f94e0bfcd916d198d9e24f54455c
tkm404 <tmcdowellddhs@yahoo.com>
2023-03-24 09:26:01 -0700

    add first survey box with JS functionality. Testing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 window.addEventListener("load", function() {
-  let form = document.querySelector("surveyText")
-  form.addEventListener("submit", function(event) {
+  let form1 = document.querySelector("dropSurvey")
+  let animal1 = document.querySelector("animal1")
+  form1.addEventListener("submit", function(event) {
     //code inside function
+    const animalSelect = document.getElementById("Animals").value;
+    animal1.removeAttribute("class");
+    document.querySelector("span#answer1").innerText = animalSelect
     event.preventDefault();
   })
 });
