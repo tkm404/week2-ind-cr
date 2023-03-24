e2877b937e14fb8a25a378a19ad0f3e4f8460451
tkm404 <tmcdowellddhs@yahoo.com>
2023-03-24 09:56:47 -0700

    BUGFIX: form1 no longer null. Fixing animal1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 window.addEventListener("load", function() {
-  let form1 = document.querySelector("dropSurvey")
-  let animal1 = document.querySelector("animal1")
+  let form1 = document.querySelector("form#dropSurvey");
+  let animal1 = document.querySelector("animal1");
   form1.addEventListener("submit", function(event) {
+    event.preventDefault();
     //code inside function
     const animalSelect = document.getElementById("Animals").value;
     animal1.removeAttribute("class");
-    document.querySelector("span#answer1").innerText = animalSelect
-    event.preventDefault();
+    document.querySelector("span#answer1").innerText = animalSelect;
   })
 });
